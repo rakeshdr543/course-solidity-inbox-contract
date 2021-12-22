768c4993a322bbf067f595ca8eb7ba99ae0093f8
Rakesh D R <rakeshdr543@gmail.com>
2021-12-22 10:54:17 +0530

    Initialized inbox smart contract repo

---
+++ contracts/inbox.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Inbox {
+    string public message;
+
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
